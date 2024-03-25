8048fdf47d756bd472e91a597fa04e2db9c4fbc2
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-25 14:30:11 +0300

    Update to dotenv package Change to using process.env for database connection parameters Add error and success messages for registration and login
    This commit updates the dotenv package to version 16.4.5 and uses process.env to set the database connection parameters. It also adds error and success messages for registration and login.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import "dotenv/config";
 import bcrypt from "bcrypt";
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -9,15 +20,6 @@ const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
-});
-db.connect();
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -36,62 +38,65 @@ app.post("/register", async (req, res) => {
   const password = req.body.password;
 
   try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
+    const checkEmail = await db.query("SELECT * FROM  users WHERE email=$1", [email]);
+    if (checkEmail.rowCount > 0) {
+      res.render("register.ejs", { error });
     } else {
-      //hashing the password and saving it in the database
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
-          console.error("Error hashing password:", err);
+          res.send("Error on hashing the password!");
         } else {
-          console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING email",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          console.log("Successfully Added new user to DATABASE!");
+          console.log(result.rows[0].email);
+          const registeredEmail = result.rows[0].email;
+          res.render("register.ejs", { registeredEmail });
         }
       });
     }
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error.message);
+    res.render("register.ejs", { error });
   }
 });
+const errorMessage = "Login credintials are wrong, Try again!";
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const loginPassword = req.body.password;
-
   try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedHashedPassword = user.password;
+    const result = await db.query("SELECT * FROM users where  email=$1", [email]);
+
+    if (result.rowCount === 0) {
+      res.render("login.ejs", { message: "Email is not registered. Please Register first." });
+    } else {
+      const storedHashedPassword = result.rows[0].password;
       bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          console.log("Error comparing passwords", err);
+          res.render("login.ejs", { message: errorMessage });
         } else {
-          if (result) {
-            res.render("secrets.ejs");
+          if (!result) {
+            res.render("login.ejs", { message: errorMessage });
+            console.log(result);
           } else {
-            res.send("Incorrect Password");
+            console.log("Sucessfully Logged In!");
+            console.log(result);
+            res.render("secrets.ejs");
           }
         }
       });
-    } else {
-      res.send("User not found");
     }
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error.message);
+    res.render("login.ejs", { error });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
